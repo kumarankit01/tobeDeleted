ad664eb4897431cc3a28393dac9415d618373062
kumar ankit <ankitax@gmail.com>
2016-11-15 18:00:40 +0530

    nc1+nc2+nc3 +...+ ncn
    In the binomial expansion
    (a+b)^n = nC0a^n. b^0 + nC1 a^(n-1)b^1 + nC2a^(n-2)b^2 + etc
    
    If you now let a=b=1
    
    (1+1)^n =2^n = nC0 + nC1 +nC2 +.   etc .   
    
    In fact what we have here is the number of subsets of size r taken from a set of n objects.
    nC0 =  1 = number of sets containing zero elements
    nC1 = n =  "               ".    "            "              1.         "
    nC2 = (n)(n-1)/2 ".    ".    ".            ".            2       ".    etc
     
    Total = nC0 + nC1+ nC2 +.    = 2^n =total number of subsets in a set of n elements.
    
    Again if a=1 and b=-1
    
    (1-1)^n =0 = nC0 - nC1 + nC2 - nC3 
    
    nC0 +n C2 + nC4 +           = nC1 + nC3 + nC3 +.
    
    = (1/2) sigma nCr  = (1/2) .2^n = 2^(n-1)

---
+++ walmart1.java
@@ -0,0 +1,18 @@
+package core;
+
+import java.util.Scanner;
+
+public class walmart1 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		int M = s.nextInt();
+		s.close();
+		double friends_combination = Math.pow(2, N) - 1 ;
+		
+		double frame_needed = Math.abs(friends_combination-M);
+		System.out.println(frame_needed);
+	}
+
+}
